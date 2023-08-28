9f385dd9164c8e8d233754cdc4af95593b02deae
tushar2704 <tushar.inseec@gmail.com>
2023-08-29 00:49:52 +0530

    MLFLOW Updates and Experiments updates

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 
 
 Dagshub
-MLFLOW_TRACKING_URI=https://dagshub.com/tushar27/MLFLOW-Project.mlflow \
+MLFLOW_TRACKING_URI=https://dagshub.com/tushar27/Wine_Quality.mlflow \
 MLFLOW_TRACKING_USERNAME=tushar27 \
 MLFLOW_TRACKING_PASSWORD=31e97a76110fac92de36585f12fb7c9ce02ea9f2 \
-python main.py
+python script.py
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from Wine_Quality import logger
-from Wine_Quality import logger
-from Wine_Quality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from Wine_Quality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-from Wine_Quality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-from Wine_Quality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-from Wine_Quality.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+from WQ import logger
+from WQ import logger
+from WQ.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from WQ.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from WQ.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from WQ.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from WQ.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
