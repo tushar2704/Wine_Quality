29de43f111d96b10a788234086249941ad69f598
tushar2704 <tushar.inseec@gmail.com>
2023-08-28 19:19:35 +0530

    Data Validation updates

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from MLFLOW import logger
 from MLFLOW import logger
 from MLFLOW.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from MLFLOW.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLFLOW.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 #from MLFLOW.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from MLFLOW.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from MLFLOW.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -18,6 +18,15 @@ except Exception as e:
         raise e
 
 
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
