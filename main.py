f4b8b3aabd7457dc33c72fed58cb29046d121998
tushar2704 <tushar.inseec@gmail.com>
2023-08-28 19:43:38 +0530

    Data Transformation updates

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MLFLOW import logger
 from MLFLOW import logger
 from MLFLOW.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLFLOW.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-#from MLFLOW.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLFLOW.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from MLFLOW.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from MLFLOW.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
@@ -29,7 +29,15 @@ except Exception as e:
         raise e
 
 
-
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
